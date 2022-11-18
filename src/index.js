11f2aff1b99ab1beb7a405cc894c46b7d37de1db
ANFADEV <32341740+ANFADEV@users.noreply.github.com>
2022-11-19 00:12:30 +0100

    Check unsupported feature
    Check if onTheSpot feature requested with watch mode
    - inform & ask user to continue without the feature

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { wrap } from 'prebuilder/src/preprocess.js';
 import { LogError, LogColor, LogWarn } from 'prebuilder/src/logger.js';
 import { parseArgs } from 'prebuilder/src/arg-manager.js';
 import { getConfigOptions } from 'prebuilder/src/config-manager.js';
+const { Confirm } = require('enquirer');
 const path = require('path');
 
 global.temp_folder = ".prebuilder-storage";
@@ -23,77 +24,94 @@ let prebuilderOptions = getConfigOptions(processInstructions.params);
 let rollupArgs = extractRollupArgs();
 
 // logging
-// console.log(JSON.stringify(processInstructions, null, '   '));
-// console.log(JSON.stringify(prebuilderOptions, null, '   '));
-// console.log(JSON.stringify(rollupArgs, null, '   '));
+// console.log(processInstructions);
+// console.log(prebuilderOptions);
+// console.log(rollupArgs);
 
 
-// watch mode
-if (processInstructions.args.some(arg => ['-w', '-watch'].includes(arg))) {
+(async function execution() {
+    // detect watch mode
+    if (processInstructions.args.some(arg => ['-w', '-watch'].includes(arg))) {
 
-    LogColor("pb-rollup:\n", 'cyan');
+        LogColor("pb-rollup:\n", 'cyan');
 
-    // onTheSpot mode unavailable in watch mode
-    prebuilderOptions.onTheSpot = false;
-
-    if (prebuilderOptions.onTheSpot) {
-        /*
-
-        This method consisted in using rollup's watch event, to implement automatic resolve/restore
-        this has been abandoned because upon watch.onEnd event's execution of restore,
-        files are changed causing a resolve/restore loop,
-
-        TLDR: this method is simpler but useless in watch mode
-
-
-        process.env.__prebuilderOutput = prebuilderOptions.scrDir;
-        // watch mode
-        if (processInstructions.args.some(arg => ['-w', '-watch'].includes(arg))) {
-
-            // rollupArgs.push(' --watch.onBundleStart \"prebuilder resolve ' + (!!processInstructions.params.config ? '-c ' + processInstructions.params.config : '') + '\"');
-            rollupArgs.push(' --watch.onStart \"prebuild resolve ' + (!!processInstructions.params.config ? '-c ' + processInstructions.params.config : '') + '\"');
-            rollupArgs.push(' --watch.onEnd \"prebuild restore\"');
-            rollupArgs.push(' --watch.onError \"prebuild restore\"');
-            rollupArgs.push(' --watch.onBundleEnd \"prebuild restore\"');
+        // onTheSpot mode unavailable in watch mode
+        if (prebuilderOptions.onTheSpot) {
+            LogColor("pb-rollup does not support --onTheSpot feature while in watch mode\n", 'yellow');
+            try {
+                await new Confirm({name: "onTheSpot feature question", message:"Ignore feature and continue?"}).run()
+                    .then(answer => {
+                        if (answer == false) {
+                            console.log("responded no");
+                            throw 'User refused to proceed';
+                        }
+                        console.log("responded yes");
+                    });
+            } catch {
+                return;
+            }
+            prebuilderOptions.onTheSpot = false;
         }
 
-        */
+        if (prebuilderOptions.onTheSpot) {
+            /*
+
+            This method consisted in using rollup's watch events, to implement automatic resolve/restore
+            this has been abandoned because upon watch.onEnd event's execution of restore,
+            copy back of files to the source folder cause a resolve/restore loop,
+
+            TLDR: this method is simpler but useless in watch mode
+
+
+            process.env.__prebuilderOutput = prebuilderOptions.scrDir;
+            // watch mode
+            if (processInstructions.args.some(arg => ['-w', '-watch'].includes(arg))) {
+
+                // rollupArgs.push(' --watch.onBundleStart \"prebuilder resolve ' + (!!processInstructions.params.config ? '-c ' + processInstructions.params.config : '') + '\"');
+                rollupArgs.push(' --watch.onStart \"prebuild resolve ' + (!!processInstructions.params.config ? '-c ' + processInstructions.params.config : '') + '\"');
+                rollupArgs.push(' --watch.onEnd \"prebuild restore\"');
+                rollupArgs.push(' --watch.onError \"prebuild restore\"');
+                rollupArgs.push(' --watch.onBundleEnd \"prebuild restore\"');
+            }
+
+            */
+        } else {
+
+            prebuilderOptions.outDir = outputPath;
+            // watch output folder
+            prebuilderOptions.watch = true;
+            // let rollup pick source from output folder & watch it
+            process.env.__prebuilderOutput = outputPath;
+            // setup rollup to watch concurrently
+            prebuilderOptions.wrap_RunCmdFirstTimeOnly = true;
+            // rollup watch mode will take on execution, so run in another process
+            prebuilderOptions.wrap_RunCmdInParallel = true;
+        }
+
+        // prebuild
+        let cmd = `rollup ${rollupArgs.join(' ')}`;
+
+        wrap(cmd, prebuilderOptions)
+            .then().catch(err => console.error(err));
+
     } else {
 
-        prebuilderOptions.outDir = outputPath;
-        // watch output folder
-        prebuilderOptions.watch = true;
-        // let rollup pick source from output folder & watch it
-        process.env.__prebuilderOutput = outputPath;
-        // setup rollup to watch concurrently
-        prebuilderOptions.wrap_RunCmdFirstTimeOnly = true;
-        // rollup watch mode will take on execution, so run in another process
-        prebuilderOptions.wrap_RunCmdInParallel = true;
+        if (prebuilderOptions.onTheSpot) {
+            process.env.__prebuilderOutput = prebuilderOptions.srcDir;
+
+        } else {
+            prebuilderOptions.outDir = outputPath;
+            // give output to rollup as source
+            process.env.__prebuilderOutput = outputPath;
+        }
+
+        // prebuild
+        let cmd = `rollup ${rollupArgs.join(' ')}`;
+
+        wrap(cmd, prebuilderOptions)
+            .then().catch(err => console.error(err));
     }
-
-    // prebuild
-    let cmd = `rollup ${rollupArgs.join(' ')}`;
-
-    wrap(cmd, prebuilderOptions)
-        .then().catch(err => console.error(err));
-
-} else {
-
-    if (prebuilderOptions.onTheSpot) {
-        process.env.__prebuilderOutput = prebuilderOptions.srcDir;
-
-    } else {
-        prebuilderOptions.outDir = outputPath;
-        // give output to rollup as source
-        process.env.__prebuilderOutput = outputPath;
-    }
-
-    // prebuild
-    let cmd = `rollup ${rollupArgs.join(' ')}`;
-
-    wrap(cmd, prebuilderOptions)
-        .then().catch(err => console.error(err));
-}
+})();
 
 
 
--- tests/no-plugin/prebuilder.config.js
+++ tests/no-plugin/prebuilder.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     srcDir: 'tests/no-plugin/src',
     log: false,
+    onTheSpot: true,
     preprocessOptions: {
         defines: [
             "MY_DIRECTIVE"
